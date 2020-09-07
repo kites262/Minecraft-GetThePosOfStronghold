0fc6e208f70e587b9c69e6f465ccb9646e9ce507
kites262 <kites262@163.com>
2020-09-07 22:46:00 +0800

    pre-GUI added

--- src/GetThePosOfStronghold.java
+++ src/GetThePosOfStronghold.java
@@ -14,50 +14,49 @@ public class GetThePosOfStronghold {
     static double deg2 = 0;
     static String value = "NULL";
     static long stime = -1;
+    static double lastime = -1;
 //    static boolean isFinished = false;
     public static void main(String[] args) {
-        logger.pl(logger.RED,"You are running version " + v);
-        logger.pl(logger.INFO,"For latest version, https://github.com/kites262/Javasr/releases");
+        logger.pl(logger.RED, "You are running version " + v);
+        logger.pl(logger.INFO, "For latest version, https://github.com/kites262/Javasr/releases");
         logger.newline();
-        for(;;) {
-            logger.pl(logger.INFO,"Now enter two sets of data:");
-            try{
-                System.out.print("->");
-                {
-                    a.x = scan.nextDouble();
-                    a.z = scan.nextDouble();
-                    deg1 = scan.nextDouble();
-                }
-                System.out.print("->");
-                {
-                    b.x = scan.nextDouble();
-                    b.z = scan.nextDouble();
-                    deg2 = scan.nextDouble();
-                }
-                stime = System.currentTimeMillis();
-                m = getdis(a,s);
-                n = getdis(b,s);
-            }catch(Exception error) {
-                logger.pl(logger.EROR,"Data error: Exist unsignificant figure(s)");
-                error.printStackTrace();
-                System.exit(1);
-            }finally {
-                s.x=
-                        a.x*sind(deg2)*cosd(deg1) - b.x*sind(deg1)*cosd(deg2) + ( a.z - b.z )*sind(deg1)*sind(deg2);
-                s.x=
-                        s.x/sind(deg2-deg1);
-                s.z=
-                        b.z*sind(deg2)*cosd(deg1) - a.z*sind(deg1)*cosd(deg2) - ( a.x - b.x )*cosd(deg1)*cosd(deg2);
-                s.z=
-                        s.z/sind(deg2-deg1);
+
+        logger.pl(logger.INFO, "Now enter two sets of data:");
+        try {
+            System.out.print("->");
+            {
+                a.x = scan.nextDouble();
+                a.z = scan.nextDouble();
+                deg1 = scan.nextDouble();
             }
+            System.out.print("->");
+            {
+                b.x = scan.nextDouble();
+                b.z = scan.nextDouble();
+                deg2 = scan.nextDouble();
+            }
+            stime = System.currentTimeMillis();
+            m = getdis(a, s);
+            n = getdis(b, s);
+        } catch (Exception error) {
+            logger.pl(logger.EROR, "Data error: Exist unsignificant figure(s)");
+            error.printStackTrace();
+            System.exit(1);
+        } finally {
+            s.x =
+                    a.x * sind(deg2) * cosd(deg1) - b.x * sind(deg1) * cosd(deg2) + (a.z - b.z) * sind(deg1) * sind(deg2);
+            s.x =
+                    s.x / sind(deg2 - deg1);
+            s.z =
+                    b.z * sind(deg2) * cosd(deg1) - a.z * sind(deg1) * cosd(deg2) - (a.x - b.x) * cosd(deg1) * cosd(deg2);
+            s.z =
+                    s.z / sind(deg2 - deg1);
+        }
         value = s.x + ", " + s.z;
         logger.pl(logger.RED, s.x + ", " + s.z);
-        logger.pl(logger.INFO,"Done!("+(System.currentTimeMillis()-stime)/1000.0+"s)");
-        logger.newline();
-        logger.newline();
-    }
-
+        lastime = ((System.currentTimeMillis() - stime) / 1000.0);
+        new ShowResult().ResultGUI("Done in "+lastime+"s",s,a,b,deg1,deg2);
+        logger.pl(logger.INFO, "Done!(" + lastime + "s)");
  }
 
     public static double sind(double deg) {
