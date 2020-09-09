eb97f0d0bdcaac86ab745acae2ab6824d9403925
kites262 <kites262@163.com>
2020-09-09 22:09:24 +0800

    v1.5

--- src/fun/calc.java
+++ src/fun/calc.java
@@ -4,40 +4,40 @@ import type.pos;
 
 public class calc {
 	public static double sind(double deg) {
-		System.out.println("[INFO]>sind operation started");
+		logger.pl(logger.INFO,"sind operation started");
 		double r =
 				Math.sin(deg / 180 * Math.PI);
-		System.out.println("[INFO]>sind operation completed");
+		logger.pl(logger.INFO,"sind operation completed");
 		return r;
 	}
 
 	public static double cosd(double deg) {
-		System.out.println("[INFO]>cosd operation started");
+		logger.pl(logger.INFO,"cosd operation started");
 		double r =
 				Math.cos(deg / 180 * Math.PI);
-		System.out.println("[INFO]>cosd operation completed");
+		logger.pl(logger.INFO,"cosd operation completed");
 		return r;
 	}
 
 	public static double getdis(pos aa, pos bb) {
-		System.out.println("[INFO]>distance operation started");
+		logger.pl(logger.INFO,"distance operation started");
 		double r =
 				rt(sq(aa.x-bb.x)+sq(aa.z-bb.z));
-		System.out.println("[INFO]>distance operation completed");
+		logger.pl(logger.INFO,"distance operation completed");
 		return r;
 	}
 
 	public static double sq(double a) {
-		System.out.println("[INFO]>pow operation started");
+		logger.pl(logger.INFO,"pow operation started");
 		double r = Math.pow(a, 2);
-		System.out.println("[INFO]>pow operation completed");
+		logger.pl(logger.INFO,"pow operation completed");
 		return r;
 	}
 
 	public static double rt(double a) {
-		System.out.println("[INFO]>rt operation started");
+		logger.pl(logger.INFO,"rt operation started");
 		double r = Math.sqrt(a);
-		System.out.println("[INFO]>rt operation completed");
+		logger.pl(logger.INFO,"rt operation completed");
 		return r;
 	}
 
